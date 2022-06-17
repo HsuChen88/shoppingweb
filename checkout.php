953cb5178221c7df3f2629ed6716ea34705a2941
dindon15456 <dindon15456@gmail.com>
2022-06-17 18:41:53 +0800

    check

--- checkout.php
+++ checkout.php
@@ -16,7 +16,7 @@
         <title>訂單已完成</title>
     </head>
     <body>
-        <div id='checkout' style='position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%);'>
+        <div id='checkout' style='position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); text-align:center'>
             <img src="/images/checkedout.jpg" alt="這是一隻郵差鳥" class="center" width="350" height="350">
             <h1>商品已經在路上囉~</h1>
             <button id="backBtn">回首頁</button>
