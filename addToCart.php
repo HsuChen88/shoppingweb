38cca91fcf7131f7af125b8a573a56af43ddf3de
dindon15456 <dindon15456@gmail.com>
2022-06-20 20:29:49 +0800

    加入購物車完成

--- addToCart.php
+++ addToCart.php
@@ -5,7 +5,10 @@
     $product_amount=$_POST['productAmount'];
     
 	    if ($user_id == "") {
-		    echo '<script>alert("請先登入會員!");</script>';
+		    echo '<script>
+            alert("請先登入會員!");
+            location.href = "./login.php";
+            </script>';
 	    }
 	    else {
 			$pdo = new PDO('sqlite:alldata.db');
@@ -15,7 +18,7 @@
 			$getCartData = $sth->fetchAll();
 			$amount = $getCartData[0][0];
 			if ($amount > 0) {
-				$amount += 1;
+				$amount += $product_amount;
 				$sth = $pdo->prepare("UPDATE Cart SET amount=$amount WHERE user_id==$user_id AND product_id==$product_id");
 				$sth->execute();
 			}
@@ -24,7 +27,9 @@
 				$sth->execute();
 			}
 			
-		echo '<script>alert("成功加入購物車!");</script>';
+            echo '<script>
+            alert("成功加入購物車!");
+            location.href = "./product.php";
+            </script>';
 		}
-    }
 ?>
