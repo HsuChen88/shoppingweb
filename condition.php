2164c50a949226e3e8e78af62254d88ab027e262
HsuChen88 <shine95763@gmail.com>
2022-07-22 22:42:05 +0800

    register check debug

--- condition.php
+++ condition.php
@@ -69,26 +69,30 @@
         preg_match($phonePattern, $phone, $phoneMatches);
         preg_match($pwdLeastPattern, $password, $pwdLeastMatches);
         preg_match($pwdSamePattern, $password, $pwdSameMatches);
+
+        var_dump($phoneMatches);
+        var_dump($pwdLeastMatches);
+        var_dump($pwdSameMatches);
         
         // 確認電話號碼有無錯誤
         if ($phoneMatches == FALSE) {
             echo '<script language="javascript">';
             echo "alert(\"請輸入正確的電話號碼\");";
-            echo "location.href='loregister.php';";
+            echo "location.href='register.php';";
             echo "</script>";
         }
         // 確認密碼長度超過8個字
         else if ($pwdLeastMatches == FALSE) {
             echo '<script language="javascript">';
             echo "alert(\"輸入的密碼未達8個字\\n請再輸入一次\");";
-            echo "location.href='loregister.php';";
+            echo "location.href='register.php';";
             echo "</script>";
         }
         // 確認兩次密碼相同
-        else if ($pwdSameMatches == FALSE) {
+        else if ($pwdSameMatches[0] == FALSE) {
             echo '<script language="javascript">';
             echo "alert(\"兩次輸入的密碼不相同\\n請再輸入一次\");";
-            echo "location.href='loregister.php';";
+            echo "location.href='register.php';";
             echo "</script>";
         }
         // 無誤 創建新用戶
